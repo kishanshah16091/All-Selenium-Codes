26026edaa39b4e6a9630435fb97f63a72dcd8fe2
kishanshah16091 <kishan.shah16091@gmail.com>
2022-04-20 00:46:03 -0400

    Screenshots

--- src/SeleniumClass1/LaunchBrowser.java
+++ src/SeleniumClass1/LaunchBrowser.java
@@ -1,4 +1,4 @@
-package com.company;
+package SeleniumClass1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
--- src/SeleniumClass1/NavigateMethod.java
+++ src/SeleniumClass1/NavigateMethod.java
@@ -1,3 +1,5 @@
+package SeleniumClass1;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
